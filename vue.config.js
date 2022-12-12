f361764d0df98abeb44458d06192d9eaf0c8652e
qq282126990 <282126990@qq.com>
2022-12-12 16:01:55 +0800

    fix:build

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,5 @@ const isProduction = process.env.NODE_ENV === 'production';
 module.exports = {
   lintOnSave: isProduction ? false : 'default',
   outputDir: 'plus',
-  publicPath: '/plus/',
+  publicPath: 'https://cdn.lovevuerk.com/plus/',
 };
