8c78846d818afe5442b30493c59cc4dbbeef3e0e
胖虎他爸爸 <37743617+JinXuanLing@users.noreply.github.com>
2022-12-26 23:56:06 +0800

    fix:progress

---
+++ src/components/progress/external.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <ivue-progress :percent="percent"></ivue-progress>
+
+        <button icon="ios-add" @click="add">1</button>
+        <button icon="ios-remove" @click="minus">2</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            percent: 0,
+        };
+    },
+    methods: {
+        add() {
+            if (this.percent >= 100) {
+                return false;
+            }
+            this.percent += 10;
+        },
+        minus() {
+            if (this.percent <= 0) {
+                return false;
+            }
+            this.percent -= 10;
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/progress/gradient.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <ivue-progress :strokeColor="['#5AB2FF', '#5B8EFF']" :percent="90"></ivue-progress>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
